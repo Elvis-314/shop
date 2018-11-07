f6e572ff52282e1789d95d2ac7276ed2e1e61b2d
yuanlu <951802133@qq.com>
2018-11-07 14:40:14 +0800

    finish the model of goods.

--- shop/settings.py
+++ shop/settings.py
@@ -36,12 +36,18 @@ AUTH_USER_MODEL = "users.UserProfile"
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'DjangoUeditor',
+    # 'users',
+    'goods',
+    'trade',
+    'user_operation',
 ]
 
 MIDDLEWARE = [
