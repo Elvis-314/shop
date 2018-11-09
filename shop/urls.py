6e133cad5b07d5fa04d832f3e93740244c9008af
yuanlu <951802133@qq.com>
2018-11-09 17:15:44 +0800

    sql: import test datas(category and product datas)

--- shop/urls.py
+++ shop/urls.py
@@ -13,14 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 # from django.urls import path
 from django.conf.urls import url, include
 import xadmin
+from shop.settings import MEDIA_ROOT
+from django.views.static import serve
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     url(r'^xadmin/', xadmin.site.urls),
     # 富文本相关url
     # path('ueditor/', include('DjangoUeditor.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT})
 ]
