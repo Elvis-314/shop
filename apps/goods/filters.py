80023beba6f2733e44b3e5e5af28dea6845c1a31
yuanlu <951802133@qq.com>
2018-12-24 18:05:14 +0800

    added: drf search and ordering

--- apps/goods/filters.py
+++ apps/goods/filters.py
@@ -11,7 +11,8 @@ class GoodsFilter(django_filters.rest_framework.FilterSet):
     """
     price_min = django_filters.NumberFilter(name="shop_price", lookup_expr='gte')
     price_max = django_filters.NumberFilter(name="shop_price", lookup_expr='lte')
-
+    # icontains 中的i，表示忽略大小写,不带lookup_expr则要求全匹配
+    # name = django_filters.CharFilter(name="name", lookup_expr='icontains')
     class Meta:
         model = Goods
         fields = ['price_min', 'price_max']
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework import filters
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -27,11 +28,12 @@ class GoodsPagination(PageNumberPagination):
 # class GoodsListView(generics.ListAPIView):
 class GoodsListViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     """
-    商品列表页
+    商品列表页，分页，过滤，搜索，排序
     """
     queryset = Goods.objects.all()
     serializer_class = GoodsSerializer
     pagination_class = GoodsPagination
-    filter_backends = (DjangoFilterBackend,)
-    filter_fields = ('name', 'shop_price')
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
     filter_class = GoodsFilter
+    search_fields = ('name', 'goods_brief', 'goods_desc')
+    ordering_fields = ('sold_num', 'add_time')
