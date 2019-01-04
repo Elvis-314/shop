d0aa8d289ac65d18490443b31ce85886ef81b50b
yuanlu <951802133@qq.com>
2019-01-04 16:45:07 +0800

    added: the shopping cart api

--- apps/trade/views.py
+++ apps/trade/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.authentication import SessionAuthentication
 
-# Create your views here.
+from .serializers import ShopCartSerializer, ShopCartDetailSerializer
+from utils.permissions import IsOwnerOrReadOnly
+from .models import ShoppingCart
+
+
+class ShoppingCartViewset(viewsets.ModelViewSet):
+    """
+    购物车功能
+    list：
+        获取购物车详情
+    create：
+        加入购物车
+    delete：
+        删除购物车
+    """
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
+    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
+    serializer_class = ShopCartSerializer
+    lookup_field = "goods_id"
+
+    # 库存数的修改
+    def perform_create(self, serializer):
+        shop_cart = serializer.save()
+        goods = shop_cart.goods
+        goods.goods_num -= shop_cart.nums
+        goods.save()
+
+    def perform_destroy(self, instance):
+        goods = instance.goods
+        goods.goods_num += instance.nums
+        goods.save()
+        instance.delete()
+
+    def perform_update(self, serializer):
+        existed_record = ShoppingCart.objects.get(id=serializer.instance.id)
+        existed_nums = existed_record.nums
+        saved_record = serializer.save()
+        nums = saved_record.nums - existed_nums
+        goods = saved_record.goods
+        goods.goods_num -= nums
+        goods.save()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ShopCartDetailSerializer
+        else:
+            return ShopCartSerializer
+
+    def get_queryset(self):
+        return ShoppingCart.objects.filter(user=self.request.user)
--- shop/urls.py
+++ shop/urls.py
@@ -29,6 +29,7 @@ from rest_framework.authtoken import views
 from goods.views import GoodsListViewSet, CategoryViewset, HotSearchsViewset
 from users.views import SmsCodeViewset, UserViewset
 from user_operation.views import UserFavViewset, LeavingMessageViewset, AddressViewset
+from trade.views import ShoppingCartViewset
 
 from rest_framework_jwt.views import obtain_jwt_token
 
@@ -58,6 +59,9 @@ router.register(r'messages', LeavingMessageViewset, base_name='messages')
 
 # 收货地址
 router.register(r'address', AddressViewset, base_name='address')
+
+# 购物车
+router.register(r'shopcarts', ShoppingCartViewset, base_name='shopcarts')
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
